2931ea37b5097f0b64666a899883d543515fec3b
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-21 11:05:28 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,8 +2,8 @@ class Backer
   attr_accessor :name
   attr_reader :backed_projects
   
-  def initialize(backer)
-    @backer = backer
+  def initialize(name)
+    @name = name
     @backed_projects = []
   end
   
